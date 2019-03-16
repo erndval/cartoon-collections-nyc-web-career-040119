71de366b905ee30adb56776797f74f044bdf0a46
Ernie Val <erndval@gmail.com>
2019-03-16 19:30:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ def roll_call_dwarves(dwarf_array)
 end
 
 def summon_captain_planet(powers_combined)
-  # Your code here
+  powers_combined.collect do |element|
+    
 end
 
 def long_planeteer_calls# code an argument here
